dc6e452f86687f0d48d0470cdbc738deb7d72831
Diluka <pmars42@gmail.com>
2013-03-05 02:48:44 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+#include"module1.h"
+#include"storage.h"
+
+storage sdata;
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+   first_face();
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
