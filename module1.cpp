2caa34d3eae731b0f87c74e9c0b6521d30a7c2f6
Diluka <pmars42@gmail.com>
2013-03-05 02:49:10 +0800

    Create module1.cpp

---
+++ module1.cpp
@@ -0,0 +1,51 @@
+#include<time.h>
+#include<iostream>
+#include<stdlib.h>
+#include"storage.h"
+
+using namespace std;
+
+void initialize(){
+   extern storage sdata;//在main中全局定义 
+	 load();
+	 //TODO
+	 }
+	 
+void welcome(){
+	 system("cls");
+	 time_t t=time(0);
+	 char time_s[64];
+	 strftime(time_s,sizeof(time_s),"%a %b %d %H:%M:%S %Y",localtime(&t));
+	 cout<<" 版权所有：Diluka，Kevin"<<endl;
+	 cout<<endl;
+	 cout<<" 学生成绩管理系统"<<endl;
+	 cout<<" 显示系统时间和日期："<<time_s<<endl;
+	 cout<<endl;
+	 cout<<"1.输入添加学生资料"<<endl;
+	 cout<<"2.显示学生资料"<<endl;
+	 cout<<"3.按学号查找"<<endl;
+	 cout<<"4.删除记录"<<endl;
+	 cout<<"5.按总分进行排序"<<endl;
+	 cout<<"6.退出"<<endl;
+	 cout<<"请选择操作(1-6)"<<endl;
+	 }
+
+char first_face(){
+	 initialize();
+	 welcome();
+
+	 char ch=getchar();
+	 while(1){
+		 switch(ch){
+		 			case'1':break;
+		 			case'2':break;
+		 			case'3':break;
+		 			case'4':break;
+		 			case'5':break;
+		 			case'6':exit(0);
+		 			default:welcome();
+					}
+		ch=getchar();
+				}
+	 return '?';
+	 }
