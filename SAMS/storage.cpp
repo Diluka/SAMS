1e272631057fa9b6c48fb521fb4d4d9f445a2552
kevinconan <kevinlyhan@gmail.com>
2013-03-06 10:42:53 +0800

    添加写文件操作storage.cpp

--- SAMS/storage.cpp
+++ SAMS/storage.cpp
@@ -1,12 +1,24 @@
-﻿#include<iostream>
+#include<iostream>
 #include"storage.h"
+#include<fstream>
 extern student_map sdata;
 bool load(){
-   /* TODO (#1#): 未实现 */
+	ifstream rfile("sdata.txt");
+	if(!rfile)
+		 return false;
+	student temp_s;
+	
+
    return true;
 	 }
 	 
 bool save(){
-	 /* TODO (#1#): 未实现 */
+	 ofstream wfile("sdata.txt");
+	 if(!wfile)
+		 return false; 
+	for(student_map::iterator iter=sdata.begin();iter!=sdata.end();iter++){
+		wfile<<(*iter).second.name<<' '<<(*iter).first<<' '<<(*iter).second.addr<<" "<<(*iter).second.score_math<<' '<<(*iter).second.score_eng<<' '<<(*iter).second.score_com<<endl;
+	}
+	wfile.close();
 	 return true;
 	 }
